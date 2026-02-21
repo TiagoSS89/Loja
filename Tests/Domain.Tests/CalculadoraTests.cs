d87c498e71b237b2fbe4702ea35b520439422de1
TiagoSS89 <tsales89@hotmail.com>
2026-02-21 17:08:03 -0300

    primeiro commit

---
+++ Tests/Domain.Tests/CalculadoraTests.cs
@@ -0,0 +1,74 @@
+﻿using Loja.Domain;
+using NUnit.Framework;
+
+namespace Domain.Tests
+{
+   
+        [TestFixture]
+        public class CalculadoraTests
+        {
+            private Calculadora _calculadora;
+
+            [SetUp]
+            public void Setup()
+            {
+                _calculadora = new Calculadora();
+            }
+
+            #region Soma
+
+            [Test]
+            public void Somar_Deve_Retornar_Resultado_Correto()
+            {
+                var resultado = _calculadora.Somar(10, 5);
+
+                Assert.That(resultado, Is.EqualTo(15));
+            }
+
+            #endregion
+
+            #region Subtração
+
+            [Test]
+            public void Subtrair_Deve_Retornar_Resultado_Correto()
+            {
+                var resultado = _calculadora.Subtrair(10, 5);
+
+                Assert.That(resultado, Is.EqualTo(5));
+            }
+
+            #endregion
+
+            #region Multiplicação
+
+            [Test]
+            public void Multiplicar_Deve_Retornar_Resultado_Correto()
+            {
+                var resultado = _calculadora.Multiplicar(10, 5);
+
+                Assert.That(resultado, Is.EqualTo(50));
+            }
+
+            #endregion
+
+            #region Divisão
+
+            [Test]
+            public void Dividir_Deve_Retornar_Resultado_Correto()
+            {
+                var resultado = _calculadora.Dividir(10, 2);
+
+                Assert.That(resultado, Is.EqualTo(5));
+            }
+
+            [Test]
+            public void Dividir_Deve_Lancar_Excecao_Quando_Dividir_Por_Zero()
+            {
+                Assert.Throws<DivideByZeroException>(() =>
+                    _calculadora.Dividir(10, 0));
+            }
+
+            #endregion
+        }
+    }
+
---
+++ src/Loja.Domain/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿namespace Loja.Domain
+{
+    public class Calculadora
+    {
+        public decimal Somar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public decimal Subtrair(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        public decimal Multiplicar(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        public decimal Dividir(decimal a, decimal b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Não é possível dividir por zero.");
+
+            return a / b;
+        }
+    }
+}
